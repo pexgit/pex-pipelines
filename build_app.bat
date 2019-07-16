7a11929c8e86edd97055eb1dbd6f2a7832dbdedf
Tola Awofolu <oawofolu@pivotal.io>
2019-07-16 13:33:11 -0500

    Added updates

--- build_app.bat
+++ build_app.bat
@@ -1,1 +1,1 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe source-code-repository/source/PexSampleAspNet/PexSampleAspNet.sln /t:restore
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe ../source-code-repository/source/PexSampleAspNet/PexSampleAspNet.sln /t:restore
