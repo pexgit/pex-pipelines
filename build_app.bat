a304ed7d5520b397fbc342a69d381232812b7480
pexgit <52937178+pexgit@users.noreply.github.com>
2019-07-17 17:42:06 -0500

    Update build_app.bat

--- build_app.bat
+++ build_app.bat
@@ -1,3 +1,4 @@
 cd source-code-repository
 nuget restore PexSampleAspNet/PexSampleAspNet/packages.config
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe PexSampleAspNet/PexSampleAspNet/PexSampleAspNet.csproj
+cd ..
