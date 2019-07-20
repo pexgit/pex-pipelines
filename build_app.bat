88ce6e38fa9b7fc330be4b21d13a69de24d8dbc8
pexgit <52937178+pexgit@users.noreply.github.com>
2019-07-19 23:03:19 -0500

    Update build_app.bat

--- build_app.bat
+++ build_app.bat
@@ -1,3 +1,11 @@
 cd source-code-repository
 nuget restore PexSampleAspNet/PexSampleAspNet/packages.config
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe PexSampleAspNet/PexSampleAspNet/PexSampleAspNet.csproj
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe PexSampleAspNet/PexSampleAspNet/PexSampleAspNet.csproj ^
+  /p:TargetFrameworkVersion=v4.7 ^
+        /t:"clean;build;publish" ^
+        /p:OutputPath="./bin/Release" ^
+        /p:Configuration=$configuration ^
+        /p:Platform=AnyCPU ^
+        /p:DeployOnBuild=true ^
+        /p:PublishDirectory="./bin/Release/Publish" ^
+        /p:PublishProfile=folderprofile
