948abc2e388445cdc8bbaf4ac664b904fe685413
pexgit <52937178+pexgit@users.noreply.github.com>
2019-07-22 20:59:51 -0400

    Update build_app.bat

--- build_app.bat
+++ build_app.bat
@@ -1,6 +1,7 @@
 cd source-code-repository
 nuget restore PexSampleAspNet/packages.config -PackagesDirectory packages
 dotnet msbuild PexSampleAspNet/PexSampleAspNet.csproj ^
+  /p:RoslynToolPath="packages\Microsoft.CodeDom.Providers.DotNetCompilerPlatform.2.0.1\build\net46\Microsoft.CodeDom.Providers.DotNetCompilerPlatform.props" ^
   /p:TargetFrameworkVersion=v4.7 ^
         /t:"clean;build;publish" ^
         /p:OutputPath="./bin/Release" ^
