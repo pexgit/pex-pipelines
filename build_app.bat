4310d91731a9787b75d764fe4edc41cb318503ff
pexgit <52937178+pexgit@users.noreply.github.com>
2019-07-24 14:14:59 -0400

    Update build_app.bat

--- build_app.bat
+++ build_app.bat
@@ -17,4 +17,4 @@ dotnet msbuild PexSampleAspNet/PexSampleAspNet.csproj ^
 dir .\PexSampleAspNet
 
 xcopy "./PexSampleAspNet/bin/Release" "../publish-directory" /O /X /E /H /K /Q
-copy "./PexSampleAspNet/Web.config" "../publish-directory"
+copy "PexSampleAspNet\Web.config" "../publish-directory/Web.config"
