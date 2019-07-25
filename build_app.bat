7a391b7f06cedd55a1d92e3a97ea1d5d7bb8b36e
pexgit <52937178+pexgit@users.noreply.github.com>
2019-07-25 05:47:01 -0400

    Update build_app.bat

--- build_app.bat
+++ build_app.bat
@@ -5,15 +5,14 @@ nuget locals all -clear
 nuget restore PexSampleAspNet/packages.config -PackagesDirectory packages
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" PexSampleAspNet/PexSampleAspNet.csproj ^
-  /t:Clean;Build;Publish ^
+  /t:Clean;Build;Package ^
   /p:TargetFrameworkVersion=v4.7 ^
         /p:OutputPath="./bin/Release" ^
         /p:Configuration=$configuration ^
         /p:Platform=AnyCPU ^
-        /p:DeployOnBuild=true ^
         /p:PublishDirectory="./bin/Release/publish" ^
         /p:PublishProfile=folderprofile
         
-dir .\PexSampleAspNet\bin\Release\publish
+dir .\PexSampleAspNet\bin\Release
 
-xcopy "./PexSampleAspNet/bin/Release/publish" "../publish-directory" /O /X /E /H /K /Q /Y
+xcopy "./PexSampleAspNet/bin/Release" "../publish-directory" /O /X /E /H /K /Q /Y
