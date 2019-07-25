66a5d022cb7ea807d53706665b0c3c32b96ff18d
pexgit <52937178+pexgit@users.noreply.github.com>
2019-07-25 05:20:37 -0400

    Update build_app.bat

--- build_app.bat
+++ build_app.bat
@@ -2,10 +2,17 @@ cd source-code-repository
 
 nuget locals all -clear
 
-nuget restore PexSampleAspNet/packages.config -PackagesDirectory packages
+C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe PexSampleAspNet/PexSampleAspNet.csproj ^
+  /t:"clean;build;publish" ^
+  /p:RoslynToolPath="packages\Microsoft.CodeDom.Providers.DotNetCompilerPlatform.2.0.1\build\net46\Microsoft.CodeDom.Providers.DotNetCompilerPlatform.props" ^
+  /p:TargetFrameworkVersion=v4.7 ^
+        /p:OutputPath="./bin/Release" ^
+        /p:Configuration=$configuration ^
+        /p:Platform=AnyCPU ^
+        /p:DeployOnBuild=true ^
+        /p:PublishDirectory="./bin/Release/publish" ^
+        /p:PublishProfile=folderprofile
         
-dotnet publish PexSampleAspNet/PexSampleAspNet.csproj -c Release -f net472 -o "./PexSampleAspNet/publish" -r win-x64
-        
-dir .\PexSampleAspNet\publish
+dir .\PexSampleAspNet\bin\Release\publish
 
-xcopy "./PexSampleAspNet/publish" "../publish-directory" /O /X /E /H /K /Q /Y
+xcopy "./PexSampleAspNet/bin/Release/publish" "../publish-directory" /O /X /E /H /K /Q /Y
